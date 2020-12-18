Fixes PPMI sparse calculation

entities/tokens_occurrence_stats.py
@@ -1,4 +1,5 @@
 from typing import List
+from scipy import sparse
 import numpy as np
 
 class TokensOccurrenceStats:
@@ -7,25 +8,22 @@ class TokensOccurrenceStats:
         sentences: List[List[int]],
         vocabulary_size: int):
 
-        self._mutual_occurrences = np.zeros((vocabulary_size, vocabulary_size))
-        self._token_occurrences = np.zeros(vocabulary_size)
+        
+        mutual_occurrences = np.zeros((vocabulary_size, vocabulary_size), dtype=np.int32)
 
         for sentence in sentences:
             for i in range(len(sentence)):
-                self._token_occurrences[sentence[i]] = self._token_occurrences[sentence[i]] + 1
 
                 if i > 0:
-                    self._mutual_occurrences[sentence[i], sentence[i-1]] = self._mutual_occurrences[sentence[i], sentence[i-1]] + 1
-                    self._mutual_occurrences[sentence[i-1], sentence[i]] = self._mutual_occurrences[sentence[i-1], sentence[i]] + 1
+                    mutual_occurrences[sentence[i], sentence[i-1]] = mutual_occurrences[sentence[i], sentence[i-1]] + 1
+                    mutual_occurrences[sentence[i-1], sentence[i]] = mutual_occurrences[sentence[i-1], sentence[i]] + 1
 
                 if i < len(sentence) - 1:
-                    self._mutual_occurrences[sentence[i], sentence[i+1]] = self._mutual_occurrences[sentence[i], sentence[i+1]] + 1
-                    self._mutual_occurrences[sentence[i+1], sentence[i]] = self._mutual_occurrences[sentence[i+1], sentence[i]] + 1
+                    mutual_occurrences[sentence[i], sentence[i+1]] = mutual_occurrences[sentence[i], sentence[i+1]] + 1
+                    mutual_occurrences[sentence[i+1], sentence[i]] = mutual_occurrences[sentence[i+1], sentence[i]] + 1
+
+        self._mutual_occurrences = sparse.dok_matrix(mutual_occurrences)
 
     @property
-    def mutual_occurrences(self):
+    def mutual_occurrences(self) -> sparse.dok_matrix:
         return self._mutual_occurrences
-        
-    @property
-    def token_occurrences(self):
-        return self._token_occurrences

models/simple/ppmi.py
@@ -36,31 +36,27 @@ class PPMI(ModelBase):
             dtype=np.float32)
 
     @overrides
-    def forward(self, stats):
+    def forward(self, stats: TokensOccurrenceStats):
         if self._initialized:
             return
 
         result = self._calculate_pmi(
-            stats[0].squeeze().numpy(),
+            stats.mutual_occurrences.todense(),
             positive=True)
 
         self._ppmi_matrix = sparse.dok_matrix(result)
 
         self._initialized = True
 
-    def _calculate_expected(
-        self,
-        matrix):
-        col_totals = matrix.sum(axis=0)
-        total = col_totals.sum()
-        row_totals = matrix.sum(axis=1)
-        return np.outer(row_totals, col_totals) / total
-
     def _calculate_pmi(
             self,
             matrix,
             positive=True):
-        matrix = matrix / self._calculate_expected(matrix)
+        col_totals = matrix.sum(axis=0)
+        total = col_totals.sum()
+        row_totals = matrix.sum(axis=1)
+        expected = np.outer(row_totals, col_totals) / total
+        matrix = matrix / expected
         # Silence distracting warnings about log(0):
         with np.errstate(divide='ignore'):
             matrix = np.log(matrix)