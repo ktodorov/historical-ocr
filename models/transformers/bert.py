f54ebbcea17a7a49f8c14f0411a0086736a4341e
ktodorov <kztodorov@outlook.com>
2021-01-26 01:02:01 +0100

    Fixes embedding extraction for huggingface BERT models

--- models/transformers/bert.py
+++ models/transformers/bert.py
@@ -38,12 +38,13 @@ class BERT(TransformerBase):
 
     @overrides
     def get_embeddings(self, tokens: List[str], vocab_ids: torch.Tensor, skip_unknown: bool = False) -> List[WordEvaluation]:
+        mask = (vocab_ids != self._arguments_service.padding_idx)
         outputs = self._transformer_model.forward(
-            vocab_ids, output_hidden_states=True)
+            vocab_ids, mask, output_hidden_states=True)
 
         # BatchSize X MaxLength X EmbeddingSize
-        padded_embeddings = outputs[1][-1]
-        mask = (vocab_ids > 0).unsqueeze(-1).repeat(1, 1, 768).float()
+        padded_embeddings = outputs[1][0]
+        mask = mask.unsqueeze(-1).repeat(1, 1, 768).float()
         means = (torch.sum(padded_embeddings * mask, dim=1) /
                  mask.sum(dim=1)).cpu().tolist()
 
